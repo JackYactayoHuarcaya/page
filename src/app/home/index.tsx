99dc74d319cd241730c8b75f87d8baf697db50c2
jack <jack-0204@hotmail.com>
2025-04-30 23:41:31 -0500

    avance_home

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,22 +1,14 @@
+import ImgHome from "./components/ImgHome";
+
 const Home = () => {
   return (
-    <div
-      className="w-full h-screen flex justify-center items-center
-       bg-slate-800"
-    >
+    <div className="w-full h-screen flex justify-center items-center">
       <div className="">
         <h1
-          className=" text-3xl cursor-default border-slate-700
-        text-slate-200 font-mono border p-5 rounded-lg"
+          className=" text-3xl cursor-default 
+        text-slate-200 font-mono p-5 rounded-lg"
         >
-          <span className="hover:text-shadow-[0_0_24px] text-shadow-blue-300">
-            App{" "}
-          </span>
-          <span>-</span>
-          <span className="hover:text-shadow-[0_0_18px] text-shadow-blue-300">
-            {" "}
-            jack
-          </span>
+          <ImgHome />
         </h1>
       </div>
     </div>
