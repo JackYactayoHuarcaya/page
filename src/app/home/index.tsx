40ed22c38c6cd3b01eed135ee9b368c4ba4569a1
jack <jack-0204@hotmail.com>
2025-05-02 22:52:48 -0500

    home update - img

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,6 +1,7 @@
 import Menu from "../../layouts/Menu";
 import Description from "./components/Description";
 import ImgHome from "./components/ImgHome";
+import ImgCode from "/code.png";
 
 const Home = () => {
   return (
@@ -10,11 +11,18 @@ const Home = () => {
         className="w-[80%] flex flex-col h-screen m-auto"
       >
         <Menu />
-        <div className="w-full h-full flex justify-center items-center">
+        <div className="mt-12 w-full h-full flex justify-center items-center">
           <div className="flex gap-4 w-full">
             <Description />
             <ImgHome />
           </div>
+        </div>
+        <div className="w-full h-full mt-6 flex justify-center">
+          <img
+            src={ImgCode}
+            className="shadow-[0_0_80px_1px_var(--bg-2-opacity)] rounded-3xl mb-3"
+            alt=""
+          />
         </div>
       </section>
     </>
